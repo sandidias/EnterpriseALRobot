543f31c7a17066853ff17f019b9d781417c69dd1
Hayaka Ryu <60289157+HayakaRyu@users.noreply.github.com>
2020-11-23 09:53:34 +0800

    Update alternate.py

--- tg_bot/modules/helper_funcs/alternate.py
+++ tg_bot/modules/helper_funcs/alternate.py
@@ -5,5 +5,5 @@ def send_message(message, text, *args, **kwargs):
     try:
         return message.reply_text(text, *args, **kwargs)
     except error.BadRequest as err:
-        if str(err) == "Reply message not found":
+        if str(err) == "Pesan balasan tidak ditemukan":
             return message.reply_text(text, quote=False, *args, **kwargs)
