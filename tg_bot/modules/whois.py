2b74cb1c620d0feb1a2f9d205a1ad2893a5b25d5
Hayaka Ryu <60289157+HayakaRyu@users.noreply.github.com>
2020-11-25 09:17:32 +0800

    Update whois.py

--- tg_bot/modules/whois.py
+++ tg_bot/modules/whois.py
@@ -19,26 +19,26 @@ def ReplyCheck(message: Message):
 
 infotext = (
     "**[{full_name}](tg://user?id={user_id})**\n"
-    " * UserID: `{user_id}`\n"
-    " * First Name: `{first_name}`\n"
-    " * Last Name: `{last_name}`\n"
-    " * Username: `{username}`\n"
-    " * Last Online: `{last_online}`\n"
+    " * ID pengguna: `{user_id}`\n"
+    " * Nama depan: `{first_name}`\n"
+    " * Nama Belakang: `{last_name}`\n"
+    " * Nama pengguna: `{username}`\n"
+    " * Terakhir Online: `{last_online}`\n"
     " * Bio: {bio}")
 
 def LastOnline(user: User):
     if user.is_bot:
         return ""
     elif user.status == 'recently':
-        return "Recently"
+        return "Baru saja"
     elif user.status == 'within_week':
-        return "Within the last week"
+        return "Dalam seminggu terakhir"
     elif user.status == 'within_month':
-        return "Within the last month"
+        return "Dalam sebulan terakhir"
     elif user.status == 'long_time_ago':
-        return "A long time ago :("
+        return "dahulu kala :("
     elif user.status == 'online':
-        return "Currently Online"
+        return "Sedang Online"
     elif user.status == 'offline':
         return datetime.fromtimestamp(user.status.date).strftime("%a, %d %b %Y, %H:%M:%S")
 
@@ -62,7 +62,7 @@ async def whois(client, message):
     try:
         user = await client.get_users(get_user)
     except PeerIdInvalid:
-        await message.reply("I don't know that User.")
+        await message.reply("Saya tidak tahu Pengguna itu.")
         return
     desc = await client.get_chat(get_user)
     desc = desc.description
@@ -74,5 +74,5 @@ async def whois(client, message):
                 last_name=user.last_name if user.last_name else "",
                 username=user.username if user.username else "",
                 last_online=LastOnline(user),
-                bio=desc if desc else "`No bio set up.`"),
+                bio=desc if desc else "`Tidak ada penyiapan biografi.`"),
             disable_web_page_preview=True)
